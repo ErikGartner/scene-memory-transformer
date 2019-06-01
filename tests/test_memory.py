1dbdab7c1608e7b457da5711d650585ec33fa3fc
Erik Gärtner <erik@gartner.io>
2019-06-01 12:47:02 +0200

    Update tests for state

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -4,17 +4,31 @@ from memory.memory import Memory
 
 
 class Test_Memory(object):
+    def setup(self):
+        self.memory_size = 10
+        self.embedding_size = 5
+        self.memory = Memory(
+            memory_size=self.memory_size, embedding_size=self.embedding_size
+        )
+
     def test_add_embedding(self):
-        memory_size = 10
-        embedding_size = 5
-        memory = Memory(memory_size=memory_size, embedding_size=embedding_size)
 
         # Test adding a single embedding
-        memory.add_embedding(np.array([1, 2, 3, 4, 5]))
-        expected = np.zeros((memory_size, embedding_size))
+        self.memory.add_embedding(np.array([1, 2, 3, 4, 5]))
+        expected = np.zeros((self.memory_size, self.embedding_size))
         expected[0, :] = np.array([1, 2, 3, 4, 5])
-        np.testing.assert_equal(expected, memory.get_state())
+        np.testing.assert_equal(expected, self.memory.get_state())
 
-        expected = np.zeros(memory_size)
+        expected = np.zeros(self.memory_size)
         expected[0] = 1
-        np.testing.assert_equal(expected, memory.get_mask())
+        np.testing.assert_equal(expected, self.memory.get_mask())
+
+        assert len(self.memory) == 1
+
+    def test_set_state(self):
+        state = np.random.rand(self.memory_size, self.embedding_size)
+        mask = np.ones(self.memory_size)
+
+        self.memory.set_state(state, mask)
+        print(self.memory.get_state())
+        print(self.memory.embeddings)
