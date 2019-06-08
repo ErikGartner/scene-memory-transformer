40cb810614ecd94bb5b6df4dabc84b3a5e2d4938
Erik Gärtner <erik@gartner.io>
2019-06-08 12:04:37 +0200

    Implement memory in tensorflow

--- memory/memory.py
+++ memory/memory.py
@@ -6,6 +6,7 @@ import typing
 from collections import deque
 
 import numpy as np
+import tensorflow as tf
 
 
 class Memory(object):
@@ -40,6 +41,17 @@ class Memory(object):
             state[:size, :] = np.array(self.embeddings)
         return state
 
+    def get_statemask(self):
+        """Returns a combined state and mask in one"""
+        size = len(self.embeddings)
+        state = np.zeros((self.memory_size, self.embedding_size + 1))
+        state[:size, :-1] = np.array(self.embeddings)
+        state[:, -1] = self.input_mask
+        return state
+
+    def set_statemask(self, statemask):
+        self.set_state(statemask[:, :-1].squeeze(), statemask[:, -1].flatten())
+
     def get_mask(self):
         return np.array(self.input_mask)
 
@@ -49,10 +61,82 @@ class Memory(object):
         :param np.ndarray emb: The new embedding.
         """
         emb = emb.squeeze()
-        assert emb.shape == (self.embedding_size,)
+        assert emb.shape == (
+            self.embedding_size,
+        ), f"{emb.shape} vs {(self.embedding_size,)}"
         self.embeddings.appendleft(emb)
         size = len(self.embeddings)
         self.input_mask[size - 1] = 1
 
     def __len__(self):
         return len(self.embeddings)
+
+
+def update_memory(observation, memory, mask, reset):
+    """
+    Update the memory and mask based on latest observation
+    """
+    assert (
+        observation.shape[0] == memory.shape[1]
+    ), f"Embedding sizes don't match, {observation.shape[0]} vs {memory.shape[1]}"
+    assert mask.shape[0] == memory.shape[0], f"Memory sizes don't match"
+    assert len(reset.shape.as_list()) == 0, f"Reset must be scalar"
+
+    reset = tf.cast(reset, dtype=tf.float32)
+
+    # Reset memory if requested
+    new_memory = memory * (1 - reset)
+
+    # Shift memory forward and add new observation
+    new_memory = tf.concat([tf.expand_dims(observation, 0), new_memory[:-1, :]], axis=0)
+
+    # Update mask
+    new_mask = mask * (1 - reset)
+    new_mask = tf.concat([tf.ones((1)), new_mask[:-1]], axis=0)
+    return new_memory, new_mask
+
+
+def batch_update_memory(observations, start_memory, start_mask, dones_ph):
+    """
+    Takes a number of observations in batch and creates appropriate memory
+    and mask for each observation.
+    """
+    assert (
+        observations.shape[0] == dones_ph.shape[0]
+    ), f"Done and observations do not match."
+
+    nbr_obs = observations.shape.as_list()[0]
+
+    masks = []
+    memories = []
+    obs = tf.split(observations, nbr_obs, axis=0)
+    dones = tf.split(dones_ph, nbr_obs, axis=0)
+    new_mem = start_memory
+    new_mask = start_mask
+    for batch_idx in range(nbr_obs):
+        new_mem, new_mask = update_memory(
+            tf.squeeze(obs[batch_idx]), new_mem, new_mask, tf.squeeze(dones[batch_idx])
+        )
+        masks.append(new_mask)
+        memories.append(new_mem)
+
+    batch_memory = tf.stack(memories, axis=0)
+    batch_mask = tf.stack(masks, axis=0)
+    assert (
+        batch_memory.shape[0] == dones_ph.shape[0]
+        and batch_memory.shape[1] == start_memory.shape[0]
+        and batch_memory.shape[2] == start_memory.shape[1]
+    ), f"Incorrect memory output shape: {batch_memory.shape}"
+    assert (
+        batch_mask.shape[0] == dones_ph.shape[0]
+        and batch_mask.shape[1] == start_mask.shape[0]
+    ), f"Incorrect mask output shape: {batch_mask.shape}"
+    return batch_memory, batch_mask
+
+
+def empty_state(memory_size, embed_size):
+    """Returns an empty state for the memory"""
+    return (
+        tf.zeros((memory_size, embed_size), dtype=np.float32),
+        tf.zeros((memory_size), dtype=np.float32),
+    )
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,6 +1,7 @@
 import numpy as np
+import tensorflow as tf
 
-from memory.memory import Memory
+from memory.memory import Memory, update_memory, batch_update_memory
 
 
 class Test_Memory(object):
@@ -32,3 +33,50 @@ class Test_Memory(object):
         self.memory.set_state(state, mask)
         np.testing.assert_equal(state, self.memory.get_state())
         np.testing.assert_equal(mask, self.memory.get_mask())
+
+
+def test_update_memory():
+    batch_size = 10
+    memory_size = 20
+    embed_size = 5
+
+    sess = tf.Session()
+    with sess:
+        observations = tf.constant(
+            np.tile(np.arange(1, batch_size + 1), (embed_size, 1)).T, dtype=tf.float32
+        )
+        memory = tf.constant(np.zeros((memory_size, embed_size), dtype=np.float32))
+        mask = tf.constant(np.zeros(memory_size, dtype=np.float32))
+        done_ph = tf.constant(np.array([0, 0, 0, 1, 0, 0, 0, 0, 1, 0]))
+
+        new_mem = memory
+        new_mask = mask
+        for idx in range(batch_size):
+
+            new_mem, new_mask = update_memory(
+                tf.squeeze(observations[idx, :]),
+                new_mem,
+                new_mask,
+                tf.squeeze(done_ph[idx]),
+            )
+
+        batch_memory, batch_mask = batch_update_memory(
+            observations, memory, mask, done_ph
+        )
+
+        mem_np, mask_np = sess.run([batch_memory, batch_mask])
+
+        corr_mem = np.zeros((3, memory_size, embed_size))
+        corr_mem[0, 0, :] = np.ones(embed_size)
+        corr_mem[1, 1, :] = np.ones(embed_size)
+        corr_mem[2, 2, :] = np.ones(embed_size)
+        corr_mem[1, 0, :] = np.ones(embed_size) * 2
+        corr_mem[2, 1, :] = np.ones(embed_size) * 2
+        corr_mem[2, 0, :] = np.ones(embed_size) * 3
+        np.testing.assert_equal(mem_np[0:3, :, :], corr_mem)
+
+        corr_mask = np.zeros((3, memory_size))
+        corr_mask[0, 0] = 1
+        corr_mask[1, 0:2] = 1
+        corr_mask[2, 0:3] = 1
+        np.testing.assert_equal(mask_np[0:3, :], corr_mask)
